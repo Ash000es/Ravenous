a39fe50559bd86afb6ea351d7014c81124e85b4a
ash000es <ash007.uk@icloud.com>
2020-03-05 22:22:28 +0100

    aa

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,9 @@ import './App.css'
 
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
-import { Yelp } from '../SearchBar/util/Yelp'
-
+import { Yelp } from '../../util/Yelp'
+const apiKey =
+  'XzUuLRpdnS_tAyeRtuiruHTo154RSFafeUSBkV5FhWXTa3NFhbJTW1ZWbPdbFCE2tu8kKmnrk0zdyrrL5pHlCwl_gbxQ1tIss6dHT_MhtVy05rdoHqvE5vH0elZSXnYx'
 class App extends React.Component {
   constructor (props) {
     super(props)
@@ -14,8 +15,38 @@ class App extends React.Component {
     this.searchYelp = this.searchYelp.bind(this)
   }
 
-  searchYelp =(term, location, sortBy) => {
-    Yelp.search(term, location, sortBy).then((res) => {
+  componentDidMount () {
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=tacos&location=london&sort_by=best_match',
+      { headers: { Authorization: `Bearer ${apiKey}` } }
+    )
+      .then(response => response.json())
+      .then(jsonResponse => {
+        console.log(jsonResponse)
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            }
+          })
+        }
+      }).then(res => {
+        this.setState({ businesses: res })
+        console.log(res)
+      })
+  }
+
+  searchYelp = (term, location, sortBy) => {
+    Yelp.search(term, location, sortBy).then(res => {
       this.setState({ businesses: res })
       console.log(res)
     })
